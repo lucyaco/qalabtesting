027e09742419939978b4bd9c9e3399548b1404e8
miuser <lucyaco29@gmail.com>
2022-08-06 23:43:15 -0500

    Nuevo envio de pc

---
+++ HolaMundo/src/main/java/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Hola, Bienvenidos a Java!!");
+        System.out.println("Hola a todos!!!");
+
+
+        int edad = 25; // =>  Ddeclaración e inicialización
+        edad = edad + 30; // => Asignacioón (cambio de valor) + operación
+
+        String nombre;   // => Declaración
+        nombre = "Jean Karlo";  // => Asignación
+
+        String cargo;   // => Declaración
+        cargo = "Docente";  // => Asignación
+
+        int nota = 100; // =>  Ddeclaración e inicialización
+        nota = nota; // => Asignacioón (cambio de valor) + operación
+
+        System.out.println("La edad es: " + edad);
+        System.out.println(nombre);
+        System.out.println(cargo);
+        System.out.println("La nota es: " + nota);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
